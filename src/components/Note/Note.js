19f2158f85686e6eb971a1cbc2b8799779832c69
Ha Thai Viet <hathaiviet411@gmail.com>
2022-03-25 13:48:02 +0700

    [FE] - Update UI.

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -44,34 +44,25 @@ const Notification = ({ data, index }) => {
     backgroundColor: '#F1F5FA'
   }
 
-  const [showDetail, setShowDetail] = useState(false);
-  const onPress = () => setShowDetail(!showDetail);
-
   return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={[styleNotification.container, index % 2 !== 0 ? bgWhite : bgBlue]}>
-          <View>
-            <Text>{ data.message }</Text>
-          </View>
-
-        <View style={styleNotification.date}>
-          <Text>{ data.date }</Text>
+    <View style={[styleNotification.container, index % 2 !== 0 ? bgWhite : bgBlue]}>
+        <View>
+          <Text style={styleNotification.message}>{ data.message }</Text>
         </View>
+
+      <View style={styleNotification.date}>
+        <Text>{ data.date }</Text>
       </View>
-
-      <View style={styleNotification.detailContainer}>
-          {
-            showDetail &&
-            <Text style={[styleNotification.detailFrame]}>{ data.message }</Text>
-          }
-        </View>
-    </TouchableOpacity>
+    </View>
   )
 }
 
 const styleNotification = StyleSheet.create({
   container: {
     padding: 15
+  },
+  message: {
+    marginBottom: 15,
   },
   date: {
     flex: 1,
@@ -80,16 +71,55 @@ const styleNotification = StyleSheet.create({
     fontSize: 11,
     alignSelf: 'flex-end'
   },
-  detailContainer: {
-    alignItems: "center",
-    padding: 10
+})
+
+const NotificationDetail = ({ data, index }) => {
+  return (
+    <View>
+        <View>
+          <Text style={styleNotificationDetail.message}>{ data.message }</Text>
+        </View>
+
+      <View style={styleNotificationDetail.date}>
+        <Text>{ data.date }</Text>
+      </View>
+
+      <View style={styleNotificationDetail.detail}>
+        <Text style={styleNotificationDetail.textDetail}>{ data.detail }</Text>
+      </View>
+    </View>
+  )
+}
+
+const styleNotificationDetail = StyleSheet.create({
+  container: {
+    padding: 15
   },
-  detailFrame: {
+  message: {
+    paddingTop: 15,
+    paddingBottom: 5,
+    paddingLeft: 5,
+  },
+  date: {
+    flex: 1,
+    color: '#000',
+    opacity: 0.5,
+    fontSize: 11,
+    alignSelf: 'flex-end',
+    paddingRight: 10,
+  },
+  detail: {
+    marginLeft: 30,
+    marginTop: 10,
+    alignItems: 'flex-start',
     borderWidth: 1,
-    borderColor: "#000000",
-    padding: 10,
-    width: 362,
+    borderColor: '#000000',
     height: 400,
+    width: 330,
+  },
+  textDetail: {
+    paddingLeft: 10,
+    paddingTop: 10,
   }
 })
 
@@ -97,15 +127,18 @@ const Note = () => {
   const data = [
     {
       date: '2022-03-01',
-      message: '今日の集会は延期となります。'
+      message: '今日の集会は延期となります。',
+      detail: 'Detail 1'
     },
     {
       date: '2022-03-02',
-      message: 'TL会議は3/3になります。'
+      message: 'TL会議は3/3になります。',
+      detail: 'Detail 2'
     },
     {
       date: '2022-03-03',
-      message: '明日の業務は台風でお休みです。'
+      message: '明日の業務は台風でお休みです。',
+      detail: 'Detail 3'
     },
   ];
 
@@ -114,6 +147,7 @@ const Note = () => {
   const [tab, setTab] = useState('TEAM');
   const [newMessage, setNewMessage] = useState('');
   const [listMessage, setListMessage] = useState(data);
+  const [messageDetail, setMessageDetail] = useState([]);
 
   function onSendMessage() {
     if (newMessage) {
@@ -128,13 +162,21 @@ const Note = () => {
     }
   }
 
+  function onPressMessage(item) {
+    if (item) {
+      setMessageDetail(item);
+    }
+  }
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <Navbar />
       <View style={styles.wrapper}>
         <View style={styles.header}>
           <View style={styles.headerIcon}>
-            <Icon style={styles.iconBack} name="angle-double-left" size={30} color="#1534A1" />
+            <Pressable onPress={() => {setTab('TEAM')}}>
+              <Icon style={styles.iconBack} name="angle-double-left" size={30} color="#1534A1" />
+            </Pressable>
           </View>
 
           <View style={styles.headerText}>
@@ -152,11 +194,11 @@ const Note = () => {
               <Text style={styles.textNotify}>1</Text>
             </View>
           </Pressable>
-          <Pressable style={[styles.tabStyle, tab === 'IZUMI' ? styles.tabActive : {}]} onPress={() => {
+          <Pressable style={[styles.tabStyle, tab === 'IZUMI'|| tab === 'IZUMI_DETAIL' ? styles.tabActive : {}]} onPress={() => {
             setTab('IZUMI');
             setNewMessage('');
           }}>
-            <Text style={[styles.tabText, tab === 'IZUMI' ? styles.tabTextActive : {}]}>Izumi</Text>
+            <Text style={[styles.tabText, tab === 'IZUMI'|| tab === 'IZUMI_DETAIL' ? styles.tabTextActive : {}]}>Izumi</Text>
             <View style={styles.messageText}>
               <Text style={styles.textNotify}>3</Text>
             </View>
@@ -197,10 +239,27 @@ const Note = () => {
                 {
                   listMessage && listMessage.map((item, index) => {
                     return (
-                      <Notification data={item} index={index + 1} key={`notification-no-${index + 1}`} />
+                      <Pressable
+                        onPress={() => {
+                          setTab('IZUMI_DETAIL');
+                          onPressMessage(item);
+                        }} 
+                        key={`press-notification-no-${index + 1}`}
+                      >
+                        <Notification data={item} index={index + 1} key={`notification-no-${index + 1}`} />
+                      </Pressable>
                     )
                   })
                 }
+              </ScrollView>
+            </View> : <View></View>
+          }
+
+          {
+            tab === 'IZUMI_DETAIL' ?
+            <View style={{ flex: 1 }}>
+              <ScrollView ref={scrollNotification} onContentSizeChange={() => scrollNotification.current.scrollToEnd({ animated: true })}>
+                <NotificationDetail data={messageDetail}></NotificationDetail>
               </ScrollView>
             </View> : <View></View>
           }
@@ -239,6 +298,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: '#0FAE71',
+    paddingTop: 5,
   },
   zoneTab: {
     flexDirection: 'row',
